47f6d65deae7bdfa0b7168dfe3e9497dfd3f7fda
zachbrown12 <zachmb22@gmail.com>
2020-03-04 16:02:23 -0500

    Test 402

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_04_155022) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "genre"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "artist_name"
+    t.string "venue_name"
+    t.date "event_date"
+    t.float "ticketmaster_price_min"
+    t.float "seatgeek_price_min"
+    t.float "price_difference_min"
+    t.float "ticketmaster_price_max"
+    t.float "seatgeek_price_max"
+    t.float "price_difference_max"
+    t.integer "artist_id"
+    t.integer "venue_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.string "username"
+    t.string "event"
+    t.integer "user_id"
+    t.integer "event_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.string "state"
+  end
 
 end
